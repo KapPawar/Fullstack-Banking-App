8a714561a9513fab12040e90cc38934376046d7f
KapPawar <pawar.kapil.shyam@gmail.com>
2024-05-06 12:08:05 -0500

    Implemented Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
-};
+  const loggedIn = {
+    firstName: "Kapil",
+    lastName: "Pawar",
+    email: "kp@email.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName}
+            subtext="Access and manage your account and transactions with ease."
+          />
 
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 560.12 }]}
+      />
+    </section>
+  );
+};
 export default Home;
